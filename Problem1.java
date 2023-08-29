6ab9155f458634355140530e4d702a4912b39163
riteshsaini01 <b210885@skit.ac.in>
2023-08-29 18:36:38 +0530

    First commit

---
+++ Problem1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int bestClosingTime(String customers) {
+        if (customers == null || customers.length()==0)
+            return 0;
+        int n = customers.length();
+        int[] prefixN = new int[n+1];
+        int[] postfixY = new int[n+1];
+        for(int i =1; i<= n; i++){
+            prefixN[i]=prefixN[i-1];
+            if (customers.charAt(i-1)=='N')
+                prefixN[i]++;
+        }
+        for(int i = n-1; i>=0; i--){
+            postfixY[i]=postfixY[i+1];
+            if (customers.charAt(i)=='Y')
+                postfixY[i]++;
+        }
+        int minPenalty = Integer.MAX_VALUE;
+        int result = 0;
+        for(int i = 0; i<=n; i++){
+            int currPenalty =prefixN[i]+postfixY[i];
+            if (currPenalty < minPenalty){
+                result = i;
+                minPenalty = currPenalty;
+            }
+        }
+        return result;
+    }
+}
